e61fd5cfdc6473d045e6b5135af8136d7edd0feb
Dustin Weatherford <dustin@causes.com>
2011-06-29 13:24:36 -0700

    Added controller index for rendering passed json.
    Began building controller logic in JS and separating the logic
    from the view. Also added a static version of jquery for when working
    offline.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => json
+      }
+    end
+  end
 end
--- public/javascripts/watch.js
+++ public/javascripts/watch.js
@@ -2,7 +2,6 @@
 
 /* TODOS
  * - We have many kinds of test objects.
- *		- special id for test and user
  *		- test has groups and users in groups
  *		- test has results storage
  *
@@ -15,49 +14,107 @@
 				// gather requested test
 				// ajax request in
 				// send response
-		// if new user, determine test placement
-		// if old user, show correct test
 		// write json and determine ways to save
 		// setup listeners
-(function() {
-	var Watcher = {
-		init : function(tests) {
-			determineUsersPlace();	
-		},
-	
-		ab_test : function(testCase, divisions) {
-			tc 	= $(testCase);
-			div	= divisions;
-	
-			if (tc && tc.length == 2) {	
-			}
+var Watcher = window.Watcher || {};
+
+Watcher.Controller = function() {
+  return {
+    unload : function unload() {
+        $.ajax({
+          type: "POST",
+          url: "/",
+          data: "super=duper&user=dustin",
+          dataType: "json"
+        });
+    }
+  }
+}();
+
+Watcher.View = function() {
+  var setAndDetermineGroup = function(t) {
+    // THIS IS RANDOM FOR NOW
+    var groupNum = Math.round(Math.random(t.groups.length));
+
+    Watcher.View.createCookie(t.name, groupNum, 30);
+    window.localStorage.setItem(t.name, groupNum); return groupNum;
+  };
+
+  return {
+    executeAB : function exAB(t) {
+      // Show user test based upon their assigned group
+      var testSubj  = $('.'+t.name);
+      var uGroup    = Watcher.View.getUserGroup(t);
+      $(testSubj[uGroup]).show();
+
+      //Setup userobj
+
+      //Send onunload
+      Watcher.Controller.unload();
+    },
+    getUserGroup : function gup(t) {
+		  var storedData 	  = window.localStorage.getItem(t.name);
+		  var storedCookie  = Watcher.View.readCookie(t.name);
+		  if (storedCookie === undefined || storedData === null) {
+        return setAndDetermineGroup(t);
+      } else {
+        return storedCookie;
+      }
+    },
+		// Methods for handling cookie storage
+    // Logic from w3schools
+    createCookie : function cc(name,value,days) {
+      var exd = new Date();
+      exd.setDate(exd.getDate() + days);
+      var val = escape(value) + ((days==null) ? 
+        "" : "; expires="+exd.toUTCString());
+      document.cookie=name + "=" + val;
+    },
+    readCookie : function rc(name) {
+      var i,x,y,ARRcookies = document.cookie.split(";");
+      for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+        x = x.replace(/^\s+|\s+$/g,"");
+        if (x == name) {
+          return unescape(y);
+        }
+      }
+    },
+		deleteCookie : function dc(name) {
+			Watcher.View.createCookie(name,"",-1);
+		}
+  }
+}();
+
+Watcher.Class = function() {
+	return {
+		init : function(t) {
+      switch(t.type) {
+        case "ab_test":
+          Watcher.View.executeAB(t);
+        default:
+          return false;
+      }
 		}
 	}
+}();
 
-	function determineUsersPlace() {
-		//Check html5 localstorage and cookies
-		//If new user, store something special
-		// and return If old user, return this.
-		console.log('running');
-	}
+var dummy = {
+	name 		: 'title_test',
+	type 		: 'ab_test',
+	groups 	: [50, 50],
+}
 
-	var dummy = {
-		name 		: 'title_test',
-		type 		: 'ab_test',
-		groups 	: [50, 50],
-	}
-	Watcher.init(dummy);
-
-})();
-
-
+Watcher.Class.init(dummy);
 
 
 
 
 
 /* IDEAS
- * - If strong computer, capture mouse movements to graph
- * - Count time on page
+ * - If strong computer, capture mouse movements to graph // sessionStorage
+ * - Count time on page // sessionStorage?
  * - Before and after urls
+ * - Time tracking
 */
