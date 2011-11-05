3dfbefa733e9d8de8b3e49380556ac72f3804178
Dustin Weatherford <dustin@causes.com>
2011-11-05 14:56:50 -0700

    God that felt good...

--- public/javascripts/watch.js
+++ public/javascripts/watch.js
@@ -19,70 +19,10 @@
 var Watcher = window.Watcher || {};
 
 Watcher.Model = function() {
-  var ABDataObj = function(g) {
-    var data = {
-      testGroup : g,
-      referrer  : document.referrer,
-      startTime : new Date(),
-    }; return data;
-  };
-
-  return {
-    init : function(dataType, group) {
-      switch(dataType) {
-        case 0 : //AB test
-          return ABDataObj(group);
-        default :
-          return false;
-      }
-    }
-  }
-}();
-
-Watcher.Controller = function() {
-  return {
-    unload : function ul(dataObj) {
-      console.log(dataObj);
-      $('.send_data').click(function() {
-        dataObj.endTime = new Date();
-        $.post("/home/watcher", dataObj, function(data) {
-          console.log(data);
-        })
-        .success(function() {})
-        .error(function() {})
-        .complete(function() {});
-      });
-    }
-  }
 }();
 
 Watcher.View = function() {
-  var setAndDetermineGroup = function(t) {
-    // THIS IS RANDOM FOR NOW
-    var groupNum = Math.round(Math.random(t.groups.length));
 
-    Watcher.View.createCookie(t.name, groupNum, 30);
-    window.localStorage.setItem(t.name, groupNum); return groupNum;
-  };
-
-  return {
-    executeAB : function exAB(t) {
-      var testSubj  = $('.'+t.name);
-      var uGroup    = Watcher.View.getUserGroup(t);
-      $(testSubj[uGroup]).show();
-
-      var dataObject = Watcher.Model.init(0, uGroup);
-      Watcher.Controller.unload(dataObject);
-    },
-    getUserGroup : function gup(t) {
-		  var storedData 	  = window.localStorage.getItem(t.name);
-		  var storedCookie  = Watcher.View.readCookie(t.name);
-		  if (storedCookie === undefined || storedData === null) {
-        return setAndDetermineGroup(t);
-      } else {
-        return storedCookie;
-      }
-    },
 		// Methods for handling cookie storage
     createCookie : function cc(name,value,days) {
       var exd = new Date();
@@ -110,18 +50,9 @@ Watcher.View = function() {
 
 Watcher.Class = function() {
 	return {
-		ABTest : function(t) {
-      Watcher.View.executeAB(t);
-		}
 	}
 }();
 
-var dummy = {
-	name 		: 'title_test',
-	groups 	: [50, 50],
-}
-
-Watcher.Class.ABTest(dummy);
 
 
 
